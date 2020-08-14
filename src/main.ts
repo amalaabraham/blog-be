569163a377a51a90c09d0708e690e22c4957c453
amalaabraham3 <amalaabraham3@gmail.com>
2020-08-14 13:01:06 +0530

    config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as config from 'config';
+
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const app = await NestFactory.create(AppModule);
 
   const options = new DocumentBuilder()
@@ -14,7 +17,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('doc', app, document);
-
-  await app.listen(3000);
+  const port = process.env.PORT || serverConfig.port;
+  await app.listen(port);
+  //await app.listen(3000);
 }
 bootstrap();
